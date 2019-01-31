f8497d87f44ea354c4f4a2c0d60e42176ba59eb3
yann <yann.boisselier@gmail.com>
2019-01-31 17:01:37 +0100

    ajout de commentaire

--- cli.js
+++ cli.js
@@ -3,19 +3,30 @@ const cli = require('@nuxt/cli')
 const path = require('path')
 
 const { PWD } = process.env
+
+// la racine du popcorn qui utilise 'popcorn-machine'
 process.env.POPCORN_DIR = PWD
+
+// le chemin vers le module 'popcorn-machine'
 process.env.POPCORN_MACHINE_DIR = __dirname
+
+// le dossier "public" du popcorn, qui sera copié dans le dossier "static"
 process.env.POPCORN_DIR_PUBLIC_PATH = path.resolve(
   process.env.POPCORN_DIR,
   'public'
 )
-process.env.POPCORN_DIR_API_PATH = path.resolve(
-  process.env.POPCORN_DIR_PUBLIC_PATH,
-  'api'
-)
+
+// le dossier du popcorn qui contiendra les fichiers markdowns à compiler
 process.env.POPCORN_DIR_CONTENT_PATH = path.resolve(
   process.env.POPCORN_DIR,
   'content'
+)
+
+// le dossier du popcorn dans lequel seront crées les fichiers JSONS compilés
+// depuis les fichiers markdowns
+process.env.POPCORN_DIR_API_PATH = path.resolve(
+  process.env.POPCORN_DIR_PUBLIC_PATH,
+  'api'
 )
 
 process.chdir(__dirname)
