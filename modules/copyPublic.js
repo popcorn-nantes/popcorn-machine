a22beec0eb9d46e69e898c72dffc0f33b9795ec9
yann <yann.boisselier@gmail.com>
2019-02-02 10:36:32 +0100

    fix dist generation and hooks

--- modules/copyPublic.js
+++ modules/copyPublic.js
@@ -1,4 +1,5 @@
 const fs = require('fs-extra')
+const consola = require('consola')
 
 const {
   POPCORN_DIR_PUBLIC_PATH,
@@ -10,10 +11,10 @@ const {
  * Copier le dossier "public" de la ville dans "static/public"
  */
 export default function copyPublic() {
-  this.nuxt.hook('modules:done', async ({ compiler }) => {
+  this.nuxt.hook('generate:done', async ({ compiler }) => {
     return copyPublicToDist()
   })
-  this.nuxt.hook('generate:before', async ({ compiler }) => {
+  this.nuxt.hook('build:done', async ({ compiler }) => {
     return copyPublicToDist()
   })
 }
@@ -23,6 +24,6 @@ function copyPublicToDist() {
   const pathB = POPCORN_DIR_DIST_PATH + '/' + POPCORN_DIR_PUBLIC_NAME
   return fs
     .copy(pathA, pathB)
-    .then(() => console.log(`dossier ${pathA} copié vers ${pathB}`))
-    .catch(e => console.log(e))
+    .then(() => consola.success(`📂 Dossier ${pathA} copié vers ${pathB}`))
+    .catch(e => consola.error(e))
 }
