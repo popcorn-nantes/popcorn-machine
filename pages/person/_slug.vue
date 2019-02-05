80dbbc6fc0acfdc5948923bc51b524090605ee40
Fabien JUIF <fabien.juif@gmail.com>
2019-02-05 14:33:58 +0100

    :construction_worker: travis (#17)
    * :construction_worker: travis
    
    * fix?<
    
    * file dependency?
    
    * relative path to cli.js
    
    * move test to script
    
    * move before to script
    
    * :ok_hand: CITY -> POPCORN_CITY

--- pages/person/_slug.vue
+++ pages/person/_slug.vue
@@ -26,7 +26,7 @@ export default {
     const title = `Profil de ${this.person.prenom} ${
       this.person.nom
     }, freelance ${this.person.technologies.join(', ')}`
-    const city = process.env.CITY ? ` à ${process.env.CITY}` : ''
+    const city = process.env.POPCORN_CITY ? ` à ${process.env.POPCORN_CITY}` : ''
     const description = `Popcorn : trouvez un développeur freelance${city} avec (vraiment) 0% de commission pour tout le monde`
     const image = `${process.env.POPCORN_BASE_URL}${this.person.photo}`
     const url = `${process.env.POPCORN_BASE_URL}${this.$route.path}`
