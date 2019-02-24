e498a2bde852b5c768c6ed66b137a70023f2fae7
yann <yann.boisselier@gmail.com>
2019-02-24 18:09:07 +0100

    Revert "ajout commande pour faire tourner les tests"
    This reverts commit 95297532b19f9f05f00f9cb7514982cdc23bb21f.

--- tests.sh
+++
@@ -1,7 +0,0 @@
-  cd test/e2e/project
-  # this is not necessary for the e2e test but we make sure generate works
-  npm run generate
-  # this is the dev version that is tested
-  npm run dev &
-  cd -
-  npm run e2e
