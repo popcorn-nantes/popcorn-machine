f918b5198f20ca6031e57daf2e24e6b8cf402e3b
yann <yann.boisselier@gmail.com>
2019-03-08 15:09:55 +0100

    Aria label

--- components/PersonsSearchForm.vue
+++ components/PersonsSearchForm.vue
@@ -2,12 +2,13 @@
   <div class="field has-text-centered">
     <p class="control has-icons-left">
       <input
+        aria-label="Rechercher un développeur par mot-clef"
         id="search"
         class="input is-medium is-flat"
         :value="value"
         @input="$emit('input', $event.target.value)"
         type="text"
-        placeholder="Cherchez par domaine (site web, application mobile, boutique...) ou technologie (React, WordPress, Java, PHP ... )"
+        placeholder="Site, Boutique, Application mobile, React, WordPress, Java, PHP ..."
       />
       <span class="icon is-small is-left"><i class="fas fa-search"></i></span>
     </p>
